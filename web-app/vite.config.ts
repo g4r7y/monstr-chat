ba7038daa88140e5e7ec373e98190763fbc45b2b
g4r7y <g4r7y@proton.me>
2026-02-09 12:07:37 +0000

    Deploy to github pages

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/monstr-chat/',
   resolve: {
     alias: [
       { find: '@core', replacement: path.resolve(__dirname, '../core/src') }

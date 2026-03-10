5cbbf774f473d285ccdcba5bedd6b069c0ef5b65
g4r7y <g4r7y@proton.me>
2026-03-10 01:08:32 +0000

    Show connection status of relays

--- web-app/src/components/Relays.tsx
+++ web-app/src/components/Relays.tsx
@@ -5,6 +5,28 @@ import { Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 import type { SettingsListener } from '@core/settingsListener';
 import { useAppView } from '../appViewContext';
 
+type RelayListProps = {
+  relays: string[];
+  connectedRelays: Set<string>;
+};
+function RelayList(props: RelayListProps) {
+  const { relays, connectedRelays } = props;
+  return (
+    <ListGroup className="mb-3">
+      {relays.map((relay, i) => (
+        <ListGroupItem key={i} className="list-group-item-secondary text-break">
+          {connectedRelays.has(relay) ? (
+            <i className="fa-solid fa-circle-check text-success me-2" aria-label="Connected"></i>
+          ) : (
+            <i className="fa-solid fa-triangle-exclamation text-warning me-2" aria-label="Disonnected"></i>
+          )}
+          {relay}
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  );
+}
+
 function Relays() {
   const controller = useChatController();
   // memoise
@@ -12,6 +34,26 @@ function Relays() {
   const { switchView } = useAppView();
 
   const [settings, setSettings] = React.useState<ChatSettings>(controller.getSettings());
+  const [connectedRelays, setConnectedRelays] = React.useState<Set<string>>(new Set());
+
+  React.useEffect(() => {
+    const checkConnections = () => {
+      const relaySet = new Set<string>();
+      const connectedRelays = controllerRef.current.checkConnectedRelays([
+        ...settings.generalRelays,
+        ...settings.inboxRelays
+      ]);
+      for (const relay of connectedRelays) {
+        relaySet.add(relay);
+      }
+      setConnectedRelays(relaySet);
+    };
+
+    checkConnections();
+    const timerId = setInterval(checkConnections, 10000);
+    const destructor = () => clearInterval(timerId);
+    return destructor;
+  }, [settings]);
 
   React.useEffect(() => {
     const listener = new (class implements SettingsListener {
@@ -33,24 +75,12 @@ function Relays() {
       <div className="row mb-3">
         <b>Message relays:</b>
       </div>
-      <ListGroup className="mb-3">
-        {settings.inboxRelays.map((relay, i) => (
-          <ListGroupItem key={i} className="list-group-item-secondary text-break">
-            {relay}
-          </ListGroupItem>
-        ))}
-      </ListGroup>
+      <RelayList relays={settings.inboxRelays} connectedRelays={connectedRelays} />
       <Button onClick={() => switchView('edit-message-relays')}>Edit</Button>
       <div className="row mb-3 mt-4">
         <b>General relays:</b>
       </div>
-      <ListGroup className="mb-3">
-        {settings.generalRelays.map((relay, i) => (
-          <ListGroupItem key={i} className="list-group-item-secondary text-break">
-            {relay}
-          </ListGroupItem>
-        ))}
-      </ListGroup>
+      <RelayList relays={settings.generalRelays} connectedRelays={connectedRelays} />
       <Button onClick={() => switchView('edit-general-relays')}>Edit</Button>
     </div>
   );
